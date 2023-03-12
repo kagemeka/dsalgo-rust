8cf3c69e514f299d5a48d14d309546e766cdc5e7
kagemeka <kagemeka1@gmail.com>
2023-03-12 08:05:51 +0000

    string searching

--- src/string_searching.rs
+++ src/string_searching.rs
@@ -1,3 +1,8 @@
+// https://en.wikipedia.org/wiki/String-searching_algorithm
+pub use crate::backward_nondeterministic_dawg_match;
+pub use crate::backward_oracle_match;
 pub use crate::boyer_moore;
+pub use crate::commentz_walter;
 pub use crate::knuth_morris_pratt;
 pub use crate::rabin_karp;
+pub use crate::two_way_string_match;
